1dba40a87ec1ef1e64ab3822392eeebbeb4964a0
wrang308 <markus.wrang_94@hotmail.com>
2018-10-09 23:03:36 +0200

    questions are now working with multiple alternatives and multiple questions

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
 var totalTime = 0
 var timerInterval
 var highScore = []
+var highScoreNames = []
+var name
+var responseURL
+var url = 'http://vhost3.lnu.se:20080/question/1'
+var multipleAnswer = false
 
 if (window.localStorage.getItem('score') === null) {
   console.log('initializing array')
@@ -10,12 +15,16 @@ if (window.localStorage.getItem('score') === null) {
 }
 
 // highScore[0] = window.prompt('New member name?')
-window.localStorage.setItem('score', JSON.stringify(highScore))
+// window.localStorage.setItem('score', JSON.stringify(highScore))
+
+highScoreNames = JSON.parse(window.localStorage.getItem('highScoreNames'))
 
 // ...
 var storedScores = JSON.parse(window.localStorage.getItem('score'))
+var storedNames = JSON.parse(window.localStorage.getItem('highScoreNames'))
 
 console.log(storedScores)
+console.log(storedNames)
 var buttonGetName = document.querySelector('#getName')
 
 buttonGetName.addEventListener('click', jsGetName)
@@ -26,20 +35,22 @@ buttonReset.addEventListener('click', jsReset)
 
 function jsReset () {
   highScore = [0, 0, 0, 0, 0]
+  highScoreNames = ['', '', '', '', '']
   window.localStorage.setItem('score', JSON.stringify(highScore))
+  window.localStorage.setItem('highScoreNames', JSON.stringify(highScoreNames))
 }
 
 function jsGetName () {
   if (totalTime === 0) {
     highScore = [0, 0, 0, 0, 0]
   }
-  var name = document.getElementById('input1').value
+  name = document.getElementById('input1').value
   var currentName = document.querySelector('#currentName')
   currentName.innerHTML = 'current name: ' + name
   console.log(name)
   console.log(totalTime)
   console.log('jsGetName = ' + totalTime)
-  addToWebStorage(totalTime)
+  addToWebStorage(totalTime, name)
   clearInterval(timerInterval)
 
   // document.location.href = 'game.html'
@@ -53,7 +64,7 @@ function totalTimeFunc () {
   }, 1000)
 }
 
-function addToWebStorage (number) {
+function addToWebStorage (number, fncName) {
   for (var i = 0; i <= 4; i++) {
     /** if (window.localStorage[i] > number) {
       console.log('number = ' + number.toString())
@@ -61,10 +72,16 @@ function addToWebStorage (number) {
       window.localStorage[i] = number
     } */
     var tmp
+    var tmpName
     if (highScore[i] < number) {
+      tmpName = highScoreNames[i]
+      highScoreNames[i] = fncName
+      fncName = tmpName
+      // s
       tmp = highScore[i]
       highScore[i] = number
       number = tmp
+      window.localStorage.setItem('highScoreNames', JSON.stringify(highScoreNames))
       window.localStorage.setItem('score', JSON.stringify(highScore))
     }
   }
@@ -83,5 +100,77 @@ function addToWebStorage (number) {
 }
  **/
 
+function initQuestion () {
+  window.fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      responseURL = data.nextURL
+      let output = '<div id="questionContainer"><h2>Question</h2>'
+      console.log(data)
+      output += data.question
+      output += '<div><input id="answer" type="text"></div>'
+      output += '<button id="submitAnswer">Submit Answer</button></div>'
+      document.getElementById('output').innerHTML = output
+      document.getElementById('submitAnswer').addEventListener('click', postAnswer)
+    })
+}
+
+function postAnswer () {
+  let answerUser
+  if (multipleAnswer) {
+    answerUser = document.querySelector('input[name="answer"]:checked').value
+  } else {
+    answerUser = document.getElementById('answer').value
+  }
+  console.log('posting answer')
+  console.log(responseURL)
+  // console.log(document.getElementById('answer').value)
+  console.log(answerUser)
+  window.fetch(responseURL, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ answer: answerUser })
+  })
+    .then(result => result.json())
+    .then(data => {
+      console.log(data)
+      if (data.message === 'Correct answer!' && data.nextURL === undefined) {
+        // add highscore
+        console.log('finished game')
+      } else if (data.message === 'Correct answer!'){
+        nextQuestion(data.nextURL)
+      }
+    })
+}
+
+function nextQuestion(startURL){
+  window.fetch(startURL)
+    .then(res => res.json())
+    .then(data => {
+      responseURL = data.nextURL
+      let output = '<div id="questionContainer"><h2>Question</h2>'
+      console.log(data)
+      output += data.question
+      if (data.alternatives !== undefined) {
+        multipleAnswer = true
+        for (var alt in data.alternatives) {
+          let radioquestion = data.alternatives[alt]
+          output += '<p>' + radioquestion + '</p>'
+          output += '<input type="radio" name="answer" value=' + alt + '>'
+        }
+      } else {
+        multipleAnswer = false
+        // a
+        output += '<div><input id="answer" type="text"></div>'
+      }
+      output += '<button id="submitAnswer">Submit Answer</button></div>'
+      document.getElementById('output').innerHTML = output
+      document.getElementById('submitAnswer').addEventListener('click', postAnswer)
+    })
+}
+
+initQuestion()
 totalTimeFunc()
 addToWebStorage()
